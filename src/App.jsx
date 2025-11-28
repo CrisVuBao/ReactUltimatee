6836227923db582075a4d9a7ecab6fdf34b16c65
vubaotech <mobile02022002@gmail.com>
2025-11-28 14:32:00 +0700

    bai tap component

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./components/todo/todo.css";
+import TodoData from "./components/todo/TodoData";
+import TodoNew from "./components/todo/TodoNew";
+import reactLogo from "./assets/react.svg";
+
+const App = () => {
+  return (
+    <div className="todo-container">
+      <div className="todo-title">Todo list</div>
+      <TodoNew />
+      <TodoData />
+      <div className="todo-image">
+        <img src={reactLogo} className="logo" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,10 @@
+const TodoNew = () => {
+  return (
+    <div className="todo-add-new">
+      <input type="text" />
+      <button>Add</button>
+    </div>
+  );
+};
+
+export default TodoNew;
